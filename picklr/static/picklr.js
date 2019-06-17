e61d4fcee099157021c9cbaaa4e4a90b33b60a04
Robert Karl <robertkarljr@gmail.com>
2019-06-17 15:32:57 -0700

    add link and clear button

--- picklr/static/picklr.js
+++ picklr/static/picklr.js
@@ -1,35 +1,40 @@
 var ENTERKEY = 13;
 var URL_PREFIX = URL_PREFIX || '';
 (function() {
-  var do_submit = function(frm) {
-    $.ajax({
-      url: URL_PREFIX + '/ratings/mh1',
-      data: frm.serialize(),
-      method: 'post',
-      success: function(data) {
-        $('#content').html('');
-        for (item in data) {
-            var name = $('<td>'+data[item][0] + '</td>');
-            var rating = $('<td>'+data[item][1] + '</td>');
-            var row = $('<tr>');
-            row.append(name);
-            row.append(rating);
-            $('#content').append(row);
-        }
-      },
-    });
-  }
-  var frm = $("#doit");
-  frm.submit(function(e) {
-    e.preventDefault();
-    do_submit(frm);
-  });
-
-  $('#cards-input').on('keypress', function(e) {
-    if (e.keyCode == ENTERKEY) {
-        do_submit(frm);
-        return true;
+    var do_submit = function(frm) {
+        $.ajax({
+            url: URL_PREFIX + '/ratings/mh1',
+            data: frm.serialize(),
+            method: 'post',
+            success: function(data) {
+                $('#content').html('');
+                for (item in data) {
+                    var name = $('<td>' + data[item][0] + '</td>');
+                    var rating = $('<td>' + data[item][1] + '</td>');
+                    var row = $('<tr>');
+                    row.append(name);
+                    row.append(rating);
+                    $('#content').append(row);
+                }
+            },
+        });
     }
-  });
-})();
+    var frm = $("#doit");
+    frm.submit(function(e) {
+        e.preventDefault();
+        do_submit(frm);
+    });
 
+    $('#cards-input').on('keypress', function(e) {
+        if (e.keyCode == ENTERKEY) {
+            do_submit(frm);
+            return true;
+        }
+    });
+
+    $('#clear-text').on('click', function(e) {
+        $('#cards-input').val('');
+    });
+
+    do_submit(frm);
+})();
